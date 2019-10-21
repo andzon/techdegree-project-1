0eb306a2332a3b1dc7772161a9f5e1d1d17c5b0d
andzon <andzonda@gmail.com>
2019-10-22 04:25:32 +0800

    Completing printquote

--- unit-01/a_random_quote_generator-v1/a_random_quote_generator-v1/inc/functions.php
+++ unit-01/a_random_quote_generator-v1/a_random_quote_generator-v1/inc/functions.php
@@ -4,49 +4,49 @@
 // Each inner array element should be an associative array
 
 $quotes[] = [
-        "quote" => "Appear weak when you are strong, and strong when you are weak.”,
-        "source" => "Sun Tzu",
-        "citation" => "everydaypower.com",
-        "year" => "2019"
+        'quote' => 'Appear weak when you are strong, and strong when you are weak.'',
+        'source' => 'Sun Tzu',
+        'citation' => 'everydaypower.com',
+        'year' => '2019'
 
 ];
 
 $quotes[] = [
-        "quote" => “Life is 10% what happens to us and 90% how we react to it.”,
-        "source" => "Dennis P. Kimbro",
-        "citation" => "keepinspiring.me",
-        "year" => "2019"
+        'quote' => 'Life is 10% what happens to us and 90% how we react to it.'',
+        'source' => 'Dennis P. Kimbro',
+        'citation' => 'keepinspiring.me',
+        'year' => '2019'
 
 ];
 
 $quotes[] = [
-        "quote" => "Every saint has a past, and every sinner has a future",
-        "source" => "scar Wilde",
-        "citation" => "wisdomquotes.com",
-        "year" => "2019"
+        'quote' => 'Every saint has a past, and every sinner has a future',
+        'source' => 'scar Wilde',
+        'citation' => 'wisdomquotes.com',
+        'year' => '2019'
 
 ];
 
 $quotes[] = [
-        "quote" => "The more I read, the more I acquire, the more certain I am that I know nothing.",
-        "source" => "Voltaire",
-        "citation" => "keepinspiring.me",
-        "year" => "2019"
+        'quote' => 'The more I read, the more I acquire, the more certain I am that I know nothing.',
+        'source' => 'Voltaire',
+        'citation' => 'keepinspiring.me',
+        'year' => '2019'
 
 ];
 
 $quotes[] = [
-        "quote" => "We shall see but a little way if we require to understand what we see.",
-        "source" => "Henry David Thoreau",
-        "citation" => "wiseoldsayings.com",
-        "year" =>  "2019"
+        'quote' => 'We shall see but a little way if we require to understand what we see.'',
+        'source' => 'Henry David Thoreau',
+        'citation' => 'wiseoldsayings.com',
+        'year' =>  "2019"
 
 ];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
-function getRandomQuote($quotes) {
+function getRandomQuote() {
 global $quotes;
 $random_number = rand(0,4);
 return $quotes[$random_number];
@@ -58,17 +58,16 @@ return $quotes[$random_number];
 function printQuote() {
 $random_quote = getRandomQuote();
 $html_string = "";
-$html_string .=  "<p class='quote'> $random_quote["quote"] </p>";
-$html_string .=  "<p class='source'> $random_quote["source"] </p>";
-if (isset($random_quote["citation"])) {
-    $html_string .=
+$html_string .=  "<p class='quote'> $random_quote['quote'] </p>";
+$html_string .=  "<p class='source'> $random_quote['source']";
+if (isset($random_quote['citation'])) {
+    $html_string .= "<span class='citation'> $random_quote['citation'] </span>";
 }
-if (isset($random_quote["year"])) {
-    $html_string .=
+if (isset($random_quote['year'])) {
+    $html_string .= "<span class='year'> $random_quote['year'] </span>";
 }
+$html_string .= "</p>";
+return $html_string;
 }
-
-var_dump($random_quote);
-
 
  ?>
