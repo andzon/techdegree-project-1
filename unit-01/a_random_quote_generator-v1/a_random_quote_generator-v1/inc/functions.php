a86d5ec890b17f3a83dc3c1d8f0967c025852ec2
andzon <andzonda@gmail.com>
2019-10-23 17:39:16 +0800

    Add tags and print color functions

--- unit-01/a_random_quote_generator-v1/a_random_quote_generator-v1/inc/functions.php
+++ unit-01/a_random_quote_generator-v1/a_random_quote_generator-v1/inc/functions.php
@@ -1,21 +1,23 @@
 <?php
 // PHP - Random Quote Generator
-// Create the Multidimensional array of quote elements and name it quotes
-// Each inner array element should be an associative array
+// $quotes is the multidimential array that contains 6 associative arrays.
+// Added additional item 'tag' into the inner quote element, going for "Exceeds Expectations" grade.
 
 $quotes[] = [
         'quote' => 'Appear weak when you are strong, and strong when you are weak.',
         'source' => 'Sun Tzu',
-        'citation' => 'everydaypower.com',
-        'year' => '2019'
+        'citation' => 'The Art of War',
+        'year' => '2005',
+        'tags' => 'Life'
 
 ];
 
 $quotes[] = [
         'quote' => 'Life is 10% what happens to us and 90% how we react to it.',
-        'source' => 'Dennis P. Kimbro',
-        'citation' => 'keepinspiring.me',
-        'year' => '2019'
+        'source' => 'Charles R. Swindoll',
+        'citation' => 'The Grace Awakening',
+        'year' => '2006',
+        'tags' => 'Attitude'
 
 ];
 
@@ -43,32 +45,52 @@ $quotes[] = [
 
 ];
 
+$quotes[] = [
+        'quote' => 'Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all.',
+        'source' => 'Dale Carnegie',
+        'citation' => ' Permission to Play',
+        'year' =>  "2003"
+
+];
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
 function getRandomQuote() {
-global $quotes;
-$random_number = rand(0,4);
-return $quotes[$random_number];
+  global $quotes;
+  $random_number = rand(0,5);
+  return $quotes[$random_number];
 }
 
 
 // Create the printQuote funtion and name it printQuote
 
 function printQuote() {
-$random_quote = getRandomQuote();
-$html_string = "";
-$html_string .=  "<p class='quote'>" . $random_quote['quote'] . "</p>";
-$html_string .=  "<p class='source'>" . $random_quote['source'];
-if (isset($random_quote['citation'])) {
+  $random_quote = getRandomQuote();
+  $html_string = "";
+  $html_string .=  "<p class='quote'>" . $random_quote['quote'] . "</p>";
+  $html_string .=  "<p class='source'>" . $random_quote['source'];
+  if (isset($random_quote['citation'])) {
     $html_string .= "<span class='citation'>" . $random_quote['citation'] . "</span>";
-}
-if (isset($random_quote['year'])) {
+  }
+  if (isset($random_quote['year'])) {
     $html_string .= "<span class='year'>" . $random_quote['year'] . "</span>";
-}
-$html_string .= "</p>";
-return $html_string;
+  }
+  if (isset($random_quote['tags'])) {
+    $html_string .= "<span class='tags'>" . $random_quote['tags'] . "</span>";
+  }
 
+  $html_string .= "</p>";
+  return $html_string;
 }
 
+function GenerateRandomColor() {
+  $background_colors = array('#282E33', '#25373A', '#164852', '#495E67', '#FF3838');
+  $random_n = rand(0,4);
+  return $background_colors[$random_n];
+}
+
+function PrintRandomColor() {
+  $random_colour = GenerateRandomColor();
+  return $random_colour;
+}
  ?>
--- unit-01/a_random_quote_generator-v1/a_random_quote_generator-v1/index.php
+++ unit-01/a_random_quote_generator-v1/a_random_quote_generator-v1/index.php
@@ -4,13 +4,14 @@
 
 <html lang="en">
 <head>
+  <meta http-equiv="refresh" content="20"/>
   <meta charset="UTF-8">
   <title>Random Quotes</title>
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/styles.css">
 </head>
-<body>
+<body backgroundcolor="<?php PrintRandomColor() ?>">
   <div class="container">
     <div id="quote-box">
      <?php echo printQuote(); ?>
